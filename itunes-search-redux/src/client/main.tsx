edc432073071c864df8686d1dd51e35cf536f738
Luke <lukemorgj@gmail.com>
2026-09-01 02:21:59 +0100

    wrap the app with the theme provider

--- itunes-search-redux/src/client/main.tsx
+++ itunes-search-redux/src/client/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import { makeStore } from './store';
+import ThemeProvider from './components/ThemeProvider';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Missing #root element');
 createRoot(container).render(
   <Provider store={makeStore()}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>,
 );
